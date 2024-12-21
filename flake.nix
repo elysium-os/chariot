5580cdc6319f60beebfefb76546701c6d55e86f9
WuX <wux@thenest.dev>
2024-12-21 20:22:29 +0200

    tools: package chariot in flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
         flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
+    outputs = { self, nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system:
         let
             pkgs = import nixpkgs { inherit system; };
             inherit (pkgs) lib stdenv mkShell fetchFromGitLab buildGoModule;
@@ -17,6 +17,24 @@
                     gdb
                     nodejs
                 ];
+            };
+
+            defaultPackage = stdenv.mkDerivation rec {
+                name = "chariot";
+                src = self;
+
+                nativeBuildInputs = with pkgs; [ gcc14 ];
+
+                installPhase = ''
+                    mkdir -p $out/bin
+                    cp chariot $out/bin/
+                '';
+
+                meta = {
+                    description = "A tool for building and bootstrapping operating systems.";
+                    homepage = https://git.thenest.dev/wux/chariot;
+                    maintainers = with lib.maintainers; [ wux ];
+                };
             };
         }
     );
