08ccdf205c5ffcff10067e17fa17ec548f7dc122
WuX <wux@thenest.dev>
2025-04-22 02:09:07 +0300

    feat: add config builder functions for runtime

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -3,7 +3,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use anyhow::{Context, Result, bail};
+use anyhow::Result;
 use child::stage1;
 use nix::unistd::{Gid, Uid};
 
@@ -33,6 +33,7 @@ pub struct Mount {
     pub is_file: bool,
 }
 
+#[allow(dead_code)]
 impl Mount {
     pub fn new(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Mount {
         Mount {
@@ -54,6 +55,7 @@ impl Mount {
     }
 }
 
+#[allow(dead_code)]
 impl RuntimeConfig {
     pub fn new(rootfs_path: impl AsRef<Path>) -> RuntimeConfig {
         RuntimeConfig {
@@ -69,6 +71,69 @@ impl RuntimeConfig {
         }
     }
 
+    pub fn set_read_only(mut self, read_only: bool) -> RuntimeConfig {
+        self.read_only = read_only;
+        self
+    }
+
+    pub fn set_network_isolation(mut self, network_isolation: bool) -> RuntimeConfig {
+        self.network_isolation = network_isolation;
+        self
+    }
+
+    pub fn set_uid(mut self, uid: Uid) -> RuntimeConfig {
+        self.uid = uid;
+        self
+    }
+
+    pub fn set_gid(mut self, gid: Gid) -> RuntimeConfig {
+        self.gid = gid;
+        self
+    }
+
+    pub fn set_cwd(mut self, cwd: impl AsRef<Path>) -> RuntimeConfig {
+        self.cwd = cwd.as_ref().to_path_buf();
+        self
+    }
+
+    pub fn set_mounts(mut self, mounts: Vec<Mount>) -> RuntimeConfig {
+        self.mounts = mounts;
+        self
+    }
+
+    pub fn set_environment(mut self, environment: HashMap<String, String>) -> RuntimeConfig {
+        self.environment = environment;
+        self
+    }
+
+    pub fn set_output_config(mut self, config: OutputConfig) -> RuntimeConfig {
+        self.output_config = Some(config);
+        self
+    }
+
+    pub fn add_mount(mut self, mount: Mount) -> RuntimeConfig {
+        self.mounts.push(mount);
+        self
+    }
+
+    pub fn add_env_var(mut self, name: String, value: String) -> RuntimeConfig {
+        self.environment.insert(name, value);
+        self
+    }
+
+    pub fn root_user(mut self) -> RuntimeConfig {
+        self.gid = Gid::from(0);
+        self.uid = Uid::from(0);
+        self
+    }
+
+    pub fn rw(mut self) -> RuntimeConfig {
+        self.read_only = false;
+        self
+    }
+}
+
+impl RuntimeConfig {
     fn relative_rootfs_path(&self, path: &str) -> PathBuf {
         match path.to_string().strip_prefix("/") {
             Some(str) => self.rootfs_path.join(Path::new(str)),
