2b6adbe65c3dff9e15c93d22259ca00a173cf618
WuX <wux@thenest.dev>
2025-04-24 01:20:42 +0300

    feat: impl timestamp and recursive copy utils from v3

--- src/util.rs
+++ src/util.rs
@@ -1,16 +1,22 @@
 use std::{
-    fs::{exists, hard_link, remove_dir_all, set_permissions},
-    os::unix::fs::PermissionsExt,
+    fs::{copy, create_dir, exists, hard_link, read_link, remove_dir_all, set_permissions},
+    os::unix::fs::{PermissionsExt, symlink},
     path::Path,
+    time::{SystemTime, UNIX_EPOCH},
 };
 
 use anyhow::{Context, Result};
+use log::warn;
 use nix::{
     libc::{S_IRWXG, S_IRWXO, S_IRWXU},
     sys::stat::Mode,
     unistd::mkdir,
 };
 use walkdir::WalkDir;
+
+pub fn get_timestamp() -> Result<u64> {
+    Ok(SystemTime::now().duration_since(UNIX_EPOCH).context("Failed to get current time")?.as_secs())
+}
 
 pub fn clean(path: impl AsRef<Path>) -> Result<()> {
     if !exists(&path)? {
@@ -48,6 +54,37 @@ pub fn link_recursive(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<()
         }
 
         hard_link(entry.path(), &dest_path).context("Failed to hard link")?;
+    }
+    Ok(())
+}
+
+pub fn copy_recursive(src: impl AsRef<Path>, dest: impl AsRef<Path>) -> Result<()> {
+    for entry in WalkDir::new(src.as_ref()).min_depth(1) {
+        let entry = &entry?;
+
+        let meta = entry.metadata().context("failed to get metadata")?;
+        let relative_path = entry.path().strip_prefix(src.as_ref()).context("failed to compute relative path")?;
+
+        let dest_path = dest.as_ref().join(relative_path);
+
+        if exists(&dest_path)? {
+            if !meta.is_dir() {
+                warn!("copy_recursive conflict on path `{}` skipping...", dest_path.to_str().unwrap());
+            }
+            continue;
+        }
+
+        if meta.is_dir() {
+            create_dir(&dest_path).context("create_dir failed")?;
+            continue;
+        }
+
+        if meta.is_symlink() {
+            symlink(read_link(entry.path())?, dest_path).context("symlink failed")?;
+            continue;
+        }
+
+        copy(entry.path(), &dest_path).context("copy failed")?;
     }
     Ok(())
 }
