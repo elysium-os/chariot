d474dae3edd9ba154958ddf314f2a19876c1c10b
WuX <wux@thenest.dev>
2025-04-27 10:23:52 +0300

    fix: add more descriptive errors to utils

--- src/util.rs
+++ src/util.rs
@@ -88,7 +88,7 @@ pub fn copy_recursive(src: impl AsRef<Path>, dest: impl AsRef<Path>) -> Result<(
         let entry = &entry?;
 
         let meta = entry.metadata().context("failed to get metadata")?;
-        let relative_path = entry.path().strip_prefix(src.as_ref()).context("failed to compute relative path")?;
+        let relative_path = entry.path().strip_prefix(src.as_ref()).context("failed to resolve relative path")?;
 
         let dest_path = dest.as_ref().join(relative_path);
 
@@ -100,16 +100,17 @@ pub fn copy_recursive(src: impl AsRef<Path>, dest: impl AsRef<Path>) -> Result<(
         }
 
         if meta.is_dir() {
-            create_dir(&dest_path).context("create_dir failed")?;
+            create_dir(&dest_path).with_context(|| format!("create_dir failed (`{}`)", dest_path.to_str().unwrap()))?;
             continue;
         }
 
         if meta.is_symlink() {
-            symlink(read_link(entry.path())?, dest_path).context("symlink failed")?;
+            symlink(read_link(entry.path())?, &dest_path)
+                .with_context(|| format!("symlink failed (`{}` -> `{}`)", entry.path().to_str().unwrap(), dest_path.to_str().unwrap()))?;
             continue;
         }
 
-        copy(entry.path(), &dest_path).context("copy failed")?;
+        copy(entry.path(), &dest_path).with_context(|| format!("copy failed (`{}` -> `{}`)", entry.path().to_str().unwrap(), dest_path.to_str().unwrap()))?;
     }
     Ok(())
 }
