6221f339603f35fe2de82b783f23d268dd7e3343
WuX <wux@thenest.dev>
2026-06-11 17:38:01 +0300

    feat(util): expose current_time as duration
    Signed-off-by: WuX <wux@thenest.dev>

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -3,11 +3,11 @@ use std::time::{Duration, SystemTime, UNIX_EPOCH};
 pub mod fs;
 pub mod lock;
 
-fn get_current_time() -> Duration {
+pub fn current_time() -> Duration {
     // Unwrap deemed safe, UNIX_EPOCH is zero, call should never fail
     SystemTime::now().duration_since(UNIX_EPOCH).unwrap()
 }
 
 pub fn current_timestamp() -> u64 {
-    get_current_time().as_secs()
+    current_time().as_secs()
 }
